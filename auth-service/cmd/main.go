5664e4d5ff351988065cecc90d99c8055945a41c
toureydaou <ttoureydaou@gmail.com>
2025-09-27 18:48:42 +0200

    updae the api lauching

--- auth-service/cmd/main.go
+++ auth-service/cmd/main.go
@@ -1,24 +1,25 @@
 package main
 
 import (
+	"auth-service/api"
 	"auth-service/internal/repository"
+	"auth-service/internal/utils"
 	"context"
 	"log"
 	"os"
 	"time"
-
-	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	router := gin.Default()
 
-	// Database connection
-	mongoUri := os.Getenv("MONGO_URL")
+	mongoUri := os.Getenv("MONGO_URI")
 	if mongoUri == "" {
 		log.Fatalf("Database URI not set")
 	}
-	repository.ConnectMongo(mongoUri)
+	client := repository.ConnectMongo(mongoUri)
+	db := client.Database(utils.AUTH_DB)
+
+	userRepo := repository.NewUserRepository(db, utils.USER_COLLECTION)
 
 	defer func() {
 		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
@@ -27,6 +28,8 @@ func main() {
 			log.Fatalf("Error while disconnecting from auth-service database %v", err)
 		}
 	}()
+
+	router := api.SetupRoutes(userRepo)
 
 	if err := router.Run(":8080"); err != nil {
 		log.Fatalf("The auth service failed to start: %v \n", err)
