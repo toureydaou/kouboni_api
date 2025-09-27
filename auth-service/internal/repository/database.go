cc2d024234e9db809bf10fd1102215fcd24a1c66
toureydaou <ttoureydaou@gmail.com>
2025-09-27 18:46:43 +0200

    update database file

--- auth-service/internal/repository/database.go
+++ auth-service/internal/repository/database.go
@@ -5,30 +5,11 @@ import (
 	"log"
 	"time"
 
-	u "auth-service/internal/models"
-
-	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var Client *mongo.Client
-
-type UserRepository interface {
-	CreateUser(ctx context.Context, user u.User) error
-	FindUserByEmail(ctx context.Context, email string) (user *u.User, error error)
-	FindUserByPhoneNumber(ctx context.Context, email string) (user *u.User, error error)
-}
-
-type userRepository struct {
-	collection *mongo.Collection
-}
-
-func NewUserRepository(db *mongo.Database, collectionName string) UserRepository {
-	return &userRepository{
-		collection: db.Collection(collectionName),
-	}
-}
 
 func ConnectMongo(uri string) *mongo.Client {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
@@ -47,36 +28,4 @@ func ConnectMongo(uri string) *mongo.Client {
 	log.Println("Connected to MongoDB!")
 	Client = client
 	return client
-}
-
-func (r *userRepository) CreateUser(ctx context.Context, user u.User) error {
-
-	_, err := r.collection.InsertOne(ctx, user)
-	return err
-
-}
-
-func (r *userRepository) FindUserByEmail(ctx context.Context, email string) (*u.User, error) {
-
-	var user *u.User
-
-	err := r.collection.FindOne(ctx, bson.M{"email": email}).Decode(&user)
-
-	if err != nil {
-		return nil, err
-	}
-
-	return user, nil
-}
-
-func (r *userRepository) FindUserByPhoneNumber(ctx context.Context, phoneNumber string) (*u.User, error) {
-	var user *u.User
-
-	err := r.collection.FindOne(ctx, bson.M{"phone_number": phoneNumber}).Decode(&user)
-
-	if err != nil {
-		return nil, err
-	}
-
-	return user, nil
 }
