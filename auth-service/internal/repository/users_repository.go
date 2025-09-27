12ecb88c1c213701f4535d84656e9ed6507338b1
toureydaou <ttoureydaou@gmail.com>
2025-09-27 18:46:13 +0200

    add users repository for register

--- auth-service/internal/repository/users_repository.go
+++ auth-service/internal/repository/users_repository.go
@@ -1,18 +1,12 @@
 package repository
 
 import (
-	"context"
-	"log"
-	"time"
-
 	u "auth-service/internal/models"
+	"context"
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 )
-
-var Client *mongo.Client
 
 type UserRepository interface {
 	CreateUser(ctx context.Context, user u.User) error
@@ -28,25 +22,6 @@ func NewUserRepository(db *mongo.Database, collectionName string) UserRepository
 	return &userRepository{
 		collection: db.Collection(collectionName),
 	}
-}
-
-func ConnectMongo(uri string) *mongo.Client {
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-
-	clientOptions := options.Client().ApplyURI(uri)
-	client, err := mongo.Connect(ctx, clientOptions)
-	if err != nil {
-		log.Fatalf("Failed to connect to MongoDB: %v", err)
-	}
-
-	if err := client.Ping(ctx, nil); err != nil {
-		log.Fatalf("MongoDB ping failed: %v", err)
-	}
-
-	log.Println("Connected to MongoDB!")
-	Client = client
-	return client
 }
 
 func (r *userRepository) CreateUser(ctx context.Context, user u.User) error {
