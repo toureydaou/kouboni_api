b502ea873b07d33f9bec94e6367be4a030ec1c7c
toureydaou <ttoureydaou@gmail.com>
2025-09-27 18:13:30 +0200

    add register unit tests

--- auth-service/tests/unit/auth_validators_test.go
+++ auth-service/tests/unit/auth_validators_test.go
@@ -1,9 +1,7 @@
-package integration
+package unit
 
 import (
-	u "auth-service/internal/models"
 	"auth-service/internal/utils"
-	"context"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -37,55 +35,7 @@ type FormatPhoneNumberTest struct {
 	PhoneNumber string `validate:"required,togolese_number"`
 }
 
-func TestUniqueEmailValidator(t *testing.T) {
-	client := SetupDB(t)
-	defer CloseDB(t, client)
-	collection := client.Database("auth_test_db").Collection("users")
-
-	utils.RegisterCustomValidators(collection)
-
-	_, _ = collection.InsertOne(context.Background(), u.User{Email: "exists@example.com"})
-
-	dto := EmailFieldTest{Email: "exists@example.com"}
-	err := utils.ValidateRegistering.Struct(dto)
-	assert.Error(t, err)
-
-	dto2 := EmailFieldTest{Email: "unique@example.com"}
-	err = utils.ValidateRegistering.Struct(dto2)
-	assert.NoError(t, err)
-}
-
-func TestUniquePhoneNumberValidator(t *testing.T) {
-	client := SetupDB(t)
-	defer CloseDB(t, client)
-
-	collection := client.Database("auth_test_db").Collection("users")
-
-	utils.RegisterCustomValidators(collection)
-
-	duplicatePhoneNumber := "90123456"
-	uniquePhoneNumber := "71234567"
-
-	_, _ = collection.InsertOne(context.Background(), u.User{PhoneNumber: duplicatePhoneNumber})
-
-	dto := PhoneNumberFieldTest{PhoneNumber: duplicatePhoneNumber}
-	err := utils.ValidateRegistering.Struct(dto)
-	assert.Error(t, err)
-
-	dto2 := PhoneNumberFieldTest{PhoneNumber: uniquePhoneNumber}
-	err = utils.ValidateRegistering.Struct(dto2)
-	assert.NoError(t, err)
-
-}
-
 func TestTogoleseNumberValidator(t *testing.T) {
-
-	client := SetupDB(t)
-	defer CloseDB(t, client)
-
-	collection := client.Database("auth_test_db").Collection("users")
-
-	utils.RegisterCustomValidators(collection)
 
 	dto := FormatPhoneNumberTest{PhoneNumber: "94012345"}
 	dto2 := FormatPhoneNumberTest{PhoneNumber: "990234"}
@@ -100,12 +50,6 @@ func TestTogoleseNumberValidator(t *testing.T) {
 }
 
 func TestNameValidator(t *testing.T) {
-	client := SetupDB(t)
-	defer CloseDB(t, client)
-
-	collection := client.Database("auth_test_db").Collection("users")
-
-	utils.RegisterCustomValidators(collection)
 
 	dto := NameFieldTest{Name: ""}
 	dto2 := NameFieldTest{Name: "1234"}
@@ -117,12 +61,6 @@ func TestNameValidator(t *testing.T) {
 }
 
 func TestSurnameValidator(t *testing.T) {
-	client := SetupDB(t)
-	defer CloseDB(t, client)
-
-	collection := client.Database("auth_test_db").Collection("users")
-
-	utils.RegisterCustomValidators(collection)
 
 	dto := SurnameFieldTest{Surname: ""}
 	dto2 := SurnameFieldTest{Surname: "1234"}
@@ -134,12 +72,6 @@ func TestSurnameValidator(t *testing.T) {
 }
 
 func TestGenderValidator(t *testing.T) {
-	client := SetupDB(t)
-	defer CloseDB(t, client)
-
-	collection := client.Database("auth_test_db").Collection("users")
-
-	utils.RegisterCustomValidators(collection)
 
 	dto := GenderFieldTest{Gender: ""}
 	dto2 := GenderFieldTest{Gender: "male"}
@@ -151,12 +83,6 @@ func TestGenderValidator(t *testing.T) {
 }
 
 func TestPasswordValidator(t *testing.T) {
-	client := SetupDB(t)
-	defer CloseDB(t, client)
-
-	collection := client.Database("auth_test_db").Collection("users")
-
-	utils.RegisterCustomValidators(collection)
 
 	dto := PasswordFieldTest{Password: ""}
 	dto2 := PasswordFieldTest{Password: "abcd"}
